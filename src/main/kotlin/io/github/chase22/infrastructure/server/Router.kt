94922275aea36f358ce5e0684f215eeb998dc73f
chase <chase@mailbox.org>
2019-10-21 22:23:46 +0200

    Migrate Router

---
+++ src/main/kotlin/io/github/chase22/infrastructure/server/Router.kt
@@ -0,0 +1,37 @@
+package io.github.chase22.infrastructure.server
+
+import io.github.chase22.infrastructure.server.endpoint.EndpointRegistry
+import io.undertow.server.HttpHandler
+import io.undertow.server.HttpServerExchange
+import io.undertow.util.AttachmentKey
+import io.undertow.util.StatusCodes.NOT_FOUND
+import org.slf4j.LoggerFactory
+import java.util.Objects.nonNull
+
+class Router(private val next: HttpHandler, private val endpointRegistry: EndpointRegistry) : HttpHandler {
+
+    @Throws(Exception::class)
+    override fun handleRequest(exchange: HttpServerExchange) {
+        val (botId, path) = exchange.requestPath.substring(1).split(['/'], 2)
+
+        exchange.putAttachment(BOT_ID_ATTACHMENT_KEY, botId)
+
+        val endpoint = endpointRegistry.getForPath(path)
+
+        if (nonNull(endpoint)) {
+            exchange.putAttachment(ENDPOINT_ATTACHMENT_KEY, endpoint)
+            next.handleRequest(exchange)
+        } else {
+            LOGGER.warn("No Endpoint for path {}", path)
+            exchange.statusCode = NOT_FOUND
+            exchange.endExchange()
+        }
+    }
+
+    companion object {
+        val ENDPOINT_ATTACHMENT_KEY: AttachmentKey<TelegramApiEndpoint<*>> = AttachmentKey.create(TelegramApiEndpoint::class.java)
+        val BOT_ID_ATTACHMENT_KEY: AttachmentKey<String> = AttachmentKey.create(String::class.java)
+
+        private val LOGGER = LoggerFactory.getLogger(Router::class.java)
+    }
+}
