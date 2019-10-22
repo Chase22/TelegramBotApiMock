4aeda36f0317f23d993ddca2ce5b2cae05e28380
chase <chase@mailbox.org>
2019-10-22 23:08:50 +0200

    Add tokenHandlerTests

--- src/test/groovy/io/github/chase22/infrastructure/server/handler/TokenHandlerSpec.groovy
+++ src/test/groovy/io/github/chase22/infrastructure/server/handler/TokenHandlerSpec.groovy
@@ -11,8 +11,6 @@ import spock.lang.Subject
 class TokenHandlerSpec extends Specification {
 
     HttpHandler handlerMock = Mock()
-    PathTemplateMatch pathTemplateMatchMock
-
 
     ServerConnection serverConnectionMock = Mock()
     HttpServerExchange exchange = new HttpServerExchange(serverConnectionMock)
@@ -20,15 +18,42 @@ class TokenHandlerSpec extends Specification {
     @Subject
     TokenHandler tokenHandler = new TokenHandler("someToken", handlerMock)
 
-    def "handleRequest() should set response code 404 if no id was supplied"() {
+    def "handleRequest() should set statusCode 404 if no id was supplied"() {
 
         when:
         tokenHandler.handleRequest(exchange)
 
-        pathTemplateMatchMock = Mock(PathTemplateMatch) {
-            getParameters() >> [:]
-        }
         then:
         exchange.getStatusCode() == StatusCodes.NOT_FOUND
+    }
+
+    def "handleRequest() should set a statusCode 403 if the given token was wrong"() {
+        given:
+        PathTemplateMatch pathTemplateMatchMock = Mock(PathTemplateMatch) {
+            getParameters() >> ["id":"someOtherToken"]
+        }
+
+        exchange.putAttachment(PathTemplateMatch.ATTACHMENT_KEY, pathTemplateMatchMock)
+
+        when:
+        tokenHandler.handleRequest(exchange)
+
+        then:
+        exchange.getStatusCode() == StatusCodes.UNAUTHORIZED
+    }
+
+    def "handleRequest should call the next handler if the token is correct"() {
+        given:
+        PathTemplateMatch pathTemplateMatchMock = Mock(PathTemplateMatch) {
+            getParameters() >> ["id":"someToken"]
+        }
+
+        exchange.putAttachment(PathTemplateMatch.ATTACHMENT_KEY, pathTemplateMatchMock)
+
+        when:
+        tokenHandler.handleRequest(exchange)
+
+        then:
+        1 * handlerMock.handleRequest(exchange)
     }
 }
